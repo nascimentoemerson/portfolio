b50d0b62d50892471ae82b5d957af22b8225ab64
nascimentoemerson <contatovemarketing.com>
2022-07-07 22:12:58 -0300

    styles

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,14 +1,28 @@
 class MobileNavbar {
-    constructor(){
+    constructor(mobileMenu, navList, navLinks) {
         this.mobileMenu = document.querySelector(mobileMenu);
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
-        this.mobileMenu.addEventListener("click", () => console.log
-        ("HEY..."));
+        this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
     init() {
@@ -20,9 +34,9 @@ class MobileNavbar {
 }
 
 const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list list"
+    ".mobile-Menu",
+    ".nav-List",
+    ".nav-List li",
 );
 
-mobileNavbar.init();
+mobileNavbar.init()
