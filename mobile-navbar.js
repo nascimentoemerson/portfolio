86ed7d07f8d9d76424a7008543b4217839dc84ad
nascimentoemerson <contatovemarketing.com>
2022-07-07 21:08:31 -0300

    css

---
+++ mobile-navbar.js
@@ -0,0 +1,28 @@
+class MobileNavbar {
+    constructor(){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => console.log
+        ("HEY..."));
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list list"
+);
+
+mobileNavbar.init();
